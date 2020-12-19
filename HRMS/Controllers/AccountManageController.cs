a1cfd6dcf61843280ae67fccadca6e1e7f4a296e
Nhi Nguyen <nhinngcs17395@fpt.edu.vn>
2020-12-19 21:56:26 +0700

    init commit

---
+++ HRMS/Controllers/AccountManageController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRMS.Controllers
+{
+    public class AccountManageController : Controller
+    {
+        // GET: AccountManage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AccountManage/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AccountManage/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AccountManage/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AccountManage/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountManage/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AccountManage/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountManage/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
